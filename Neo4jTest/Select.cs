c71b1c942c4ae4cbe4675df475598d3a2f81ab2a
11tmazhari <tim.mazhari@sonova.com>
2019-11-19 19:18:11 +0100

    Add project files.

---
+++ Neo4jTest/Select.cs
@@ -0,0 +1,61 @@
+﻿using Neo4j.Driver.V1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Neo4jTest
+{
+    class Select
+    {
+            
+            public void getMoviesOfActor(string name, string lastname) {
+
+                var driver = GraphDatabase.Driver("bolt://localhost", AuthTokens.Basic("neo4j", "root"));
+                var session = driver.Session();
+
+                string statement = "MATCH(p:Person{name:'" + name + "', lastname:'" + lastname + "'})-[:ACTED_IN]->(m:Movie) return m";
+                IStatementResult result = session.Run(statement);
+
+                INode node = null;
+                string title;
+                string tagline;
+                Console.WriteLine(name + " " + lastname + " Played in: ");
+                foreach (var record in result)
+                {
+                    node = record["m"].As<INode>();
+                    title = node["title"].As<string>();
+                    tagline = node["tagline"].As<string>();
+                Console.WriteLine(title + ": " + tagline);
+                }
+
+            }
+            
+            public void getActorsOfMovie(string title)
+            {
+
+                var driver = GraphDatabase.Driver("bolt://localhost", AuthTokens.Basic("neo4j", "root"));
+                var session = driver.Session();
+
+                string statement = "MATCH(m:Movie{title:'" + title + "'})<-[:ACTED_IN]-(p:Person) return p";
+
+                IStatementResult result = session.Run(statement);
+
+                INode node = null;
+                string name;
+                string nachname;
+                int born;
+
+            Console.WriteLine("The following actors play in " + title + " : ");
+                foreach(var record in result)
+                {
+                    node = record["p"].As<INode>();
+                    name = node["name"].As<string>();
+                    nachname = node["lastname"].As<string>();
+                    born = node["born"].As<int>();
+
+                    Console.WriteLine(name + " " + nachname + ", " + born);
+                }
+
+            }
+    }
+}
---
+++ Neo4jTest/Yeet.cs
@@ -0,0 +1,19 @@
+﻿using Neo4j.Driver.V1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Neo4jTest
+{
+    public class Yeet
+    {
+        static void Main(string[] args)
+        {
+            Select select = new Select();
+            //select.getMoviesOfActor("Tom", "Hanks");
+            select.getActorsOfMovie("The Da Vinci Code");
+
+        }
+    }
+}
